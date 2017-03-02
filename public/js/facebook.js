72771a7d989b87553a0c5b5d45109897a81fa68d
Akshay Mahajan <a1mahaja@ucsd.edu>
2017-03-02 11:17:12 -0800

    Now working with FB API

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -11,9 +11,18 @@ function statusChangeCallback(response) {
   // app know the current login status of the person.
   // Full docs on the response object can be found in the documentation
   // for FB.getLoginStatus().
+ 
   if (response.status === 'connected') {
     // Logged into your app and Facebook.
         console.log('Successfully logged in with Facebook');
          FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
   }
 }
+
+function changeUser(response) {
+	console.log(response);
+	$('.facebookLogin').hide();
+	$('#name').text(response.name);
+	$('#photo').attr('src',"https://scontent.xx.fbcdn.net/v/t1.0-1/p480x480/13006742_10204968937457781_1971302455557520269_n.jpg?oh=90a71f7ad8e0909233580af4e3bbcda4&oe=593D51AD" );
+}
+
